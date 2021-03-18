14a681bb7a13840c674fa89c2c25514c45292200
Mario González <mario@mariogl.com>
2021-03-18 12:52:05 +0100

    Ampliación enunciado para incluir enrutado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="contenedor">
-      <header className="cabecera">
+      {/* <header className="cabecera">
         <h1>Parada nº 15</h1>
         <div className="display">
           <div className="bus">
@@ -34,6 +34,21 @@ function App() {
             <option value="">Elige línea</option>
           </select>
         </form>
+      </section> */}
+      <header className="cabecera">
+        <h2>Bus 109 - Hospital Clínic / Polígon Zona Franca</h2>
+        <h3>Polígon Zona Franca -> Hospital Clínic</h3>
+        <a href="#">Volver a la portada</a>
+      </header>
+      <section>
+        <ul className="grafico-paradas">
+          <li className="parada">Parada nº X: Nombre parada (<a href="#">ver mapa</a>)</li>
+          <li className="parada">Parada nº 1741: Cotxeres TB Zona Franca (<a href="#">ver mapa</a>)</li>
+          <li className="parada">Parada nº 1045: Pg Zona Franca - Motors (<a href="#">ver mapa</a>)</li>
+          <li className="parada">Parada nº 1615: Carrer número 4 - Carrer D (<a href="#">ver mapa</a>)</li>
+          <li className="parada">Parada nº 1639: Carrer A- Comissaria Portuària (<a href="#">ver mapa</a>)</li>
+          <li className="parada">Parada nº 1643: Mercabarna - Mercat del Peix (<a href="#">ver mapa</a>)</li>
+        </ul>
       </section>
     </div>
   );
